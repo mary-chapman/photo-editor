e68453f73583b9b86a74705a45330924c7e73870
mary-chapman <mbchapman02@gmail.com>
2018-06-03 21:25:01 -0400

    version 2, handlesave, handleclick and handleinput are working

--- src/components/EditPhoto.js
+++ src/components/EditPhoto.js
@@ -7,76 +7,116 @@ import './EditPhoto.css';
 
 
 
+
 class EditPicture extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-            activeFilter: 'grayscale',
-            filterValue: .5
+            savedFilters: [],
+            currentFilterName: '',
+            currentFilterValue: '',
+            currentFilterUnit: '',
+            currentFilterMin: '',
+            currentFilterMax: '',
+            currentFilterStep: '',
+            currentFilterValue: ''
         };
 
         this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
     }
+
+    handleSave() {
+        var filterToSave = `${this.state.currentFilterName}(${document.getElementById("sliderInput").value}${this.state.currentFilterUnit})`
+        this.setState({ savedFilters: [...this.state.savedFilters, filterToSave]})
+        //document.getElementById("image").style.filter += filterToSave;
+        document.getElementById("text").innerHTML = 'filter: ' + this.state.savedFilters.toString() + ' ' + filterToSave;
+
+
+        console.log("ON save " + (Number(this.state.currentFilterValue)))   
+     }
 
     handleClick(e) {
-        document.getElementsByClassName("sliderInput")[0].min =  filters[e.target.className].min
-        document.getElementsByClassName("sliderInput")[0].max =  filters[e.target.className].max
-        document.getElementsByClassName("sliderInput")[0].value =  filters[e.target.className].defaultValue
-        document.getElementsByClassName("image")[0].setAttribute(`style`, `filter:${e.target.className}(${filters[e.target.className].defaultValue + filters[e.target.className].unit})`);
-
+        const filterClicked = filters[e.target.className];
         this.setState({
-            activeFilter: e.target.className,
-            filterValue: filters[e.target.className].defaultValue
+            currentFilterName: filterClicked.name,
+            currentFilterValue: filterClicked.defaultValue,
+            currentFilterUnit: filterClicked.unit,
+            currentFilterMin: filterClicked.min,
+            currentFilterMax: filterClicked.max,
+            currentFilterStep: filterClicked.step
         })
-        console.log("input value: " + document.getElementsByClassName("sliderInput")[0].value)
-        console.log("default value: " + filters[e.target.className].defaultValue)
+        if (this.state.savedFilters.length > 0) {
+            document.getElementById("image").style.filter = `${this.state.savedFilters.toString()} ${filterClicked.name}(${filterClicked.defaultValue}${filterClicked.unit})`;
+        } else {
+            document.getElementById("image").style.filter = `${filterClicked.name}(${filterClicked.defaultValue}${filterClicked.unit})`;
+        }
         
+        document.getElementById("sliderInput").min = filterClicked.min;
+        document.getElementById("sliderInput").max = filterClicked.max;
+        document.getElementById("sliderInput").step = filterClicked.step;
+        document.getElementById("sliderInput").value = filterClicked.defaultValue;
+
+        document.getElementById("text").innerHTML = `filter: ${this.state.savedFilters.toString()} ${filterClicked.name}(${filterClicked.defaultValue}${filterClicked.unit})`
     }
 
+    async handleChange() {
+        var sliderInput = document.getElementById("sliderInput");
+        var sliderVaue = sliderInput.value;
+        
+        // set the min, max and step of the slider 
+        sliderInput.min = filters[this.state.currentFilterName].min;
+        sliderInput.max = filters[this.state.currentFilterName].max;
+        sliderInput.step = filters[this.state.currentFilterName].step;
 
+        // change the picture to the value of the slider
+        document.getElementById("image").style.filter = `${this.state.savedFilters.toString()} ${this.state.currentFilterName}(${sliderVaue}${this.state.currentFilterUnit})`;
 
-    handleChange() {
-        this.setState({
-            filterValue: document.getElementsByClassName("sliderInput")[0].value + filters[this.state.activeFilter].unit
+        //console.log(`${this.state.savedFilters.toString()} ${this.state.currentFilterName}(${sliderVaue}${this.state.currentFilterUnit})`);
+        // change the state 
+        await this.setState({
+            currentFilterName: filters[this.state.currentFilterName].name,
+            currentFilterValue: sliderVaue,
+            currentFilterUnit: filters[this.state.currentFilterName].unit,
+            currentFilterMin: filters[this.state.currentFilterName].min,
+            currentFilterMax: filters[this.state.currentFilterName].max,
+            currentFilterStep: filters[this.state.currentFilterName].step
         })
-        document.getElementsByClassName("image")[0].setAttribute(`style`, `filter:${this.state.activeFilter}(${this.state.filterValue})`);        
-        console.log(this.state.filterValue);
+
+        document.getElementById("text").innerHTML = await `filter: ${this.state.savedFilters.toString()} ${this.state.currentFilterName}(${this.state.currentFilterValue}${this.state.currentFilterUnit})`
+        console.log("ON CHANGE" + this.state.currentFilterValue)
     }
+
 
     render() {
-        let tempImage = '../assets/cat.jpg'
+        let tempImage = 'https://images.unsplash.com/photo-1505325681473-5e712b2eadca?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42268883c25739c07af3d28d8f2030de&auto=format&fit=crop&w=1867&q=80'
 
         return (
             <div className="editPicture">
-          
-                <img className="image" src={require('../assets/cat.jpg')} width="400px" alt="" />
-                
+
+                {/* <img id="image" src={require('../assets/cat.jpg')} width="400px" alt="" /> */}
+                <img id="image" src={tempImage} width="400px" alt="" />
+
                 <div className="code">
-                    <div className="text">filter: {this.state.activeFilter}({this.state.filterValue}) </div>
+                    <div id="text"></div>
                 </div>
 
                 <div className="slidecontainer">
-                    <input onChange={this.handleChange} 
-                        className="sliderInput" 
-                        type="range" 
-                        min={filters[this.state.activeFilter].min} 
-                        max={filters[this.state.activeFilter].max} 
-                        defaultValue={filters[this.state.activeFilter].detaultValue} 
-                        step={filters[this.state.activeFilter].step }/>
-                    <button className="save">save</button>
+                <input type="range"  id="sliderInput" onChange = {this.handleChange} />
+                <button className="save" onClick={this.handleSave}>save</button>
                 </div>
 
-                {/* buttons */}
+
                 <div className="filters">
                     <button className="grayscale" onClick={this.handleClick}>grayscale</button>
                     <button className="saturate" onClick={this.handleClick}>saturate</button>
                     <button className="blur" onClick={this.handleClick}>blur</button>
-                    <button onClick={this.handleClick}>sepia</button>
-                    <button onClick={this.handleClick}>hue-rotate</button>
+                    <button className="sepia" onClick={this.handleClick}>sepia</button>
+                    <button className="hueRotate" onClick={this.handleClick}>hue-rotate</button>
                     <button onClick={this.handleClick}>brightness</button>
                     <button onClick={this.handleClick}>contrast</button>
-                    <button onClick={this.handleClick}>invert</button>
+                    <button className="invert" onClick={this.handleClick}>invert</button>
                     <button onClick={this.handleClick}>opacity</button>
                 </div>
             </div>
--- src/filterData.js
+++ src/filterData.js
@@ -1,26 +1,51 @@
 const filters = {
     grayscale: {
+        name: 'grayscale',
         min: 0,
         max: 1,
         step: .01,
         unit: "",
-        defaultValue: 0.5,
+        defaultValue: 1,
     },
     saturate: {
+        name: 'saturate',
         min: 0,
-        max: 500,
+        max: 400,
         step: 1,
         unit: "%",
-        defaultValue: 250,
+        defaultValue: 200,
     },
     blur: {
+        name: 'blur',
         min: 0,
         max: 10,
         step: .01,
         unit: "px",
         defaultValue: 5,
     },
-    
+    sepia: {
+        name: 'sepia',
+        min: 0,
+        max: 1,
+        step: .01,
+        defaultValue: .5,
+        unit: ""
+    },
+    hueRotate: {
+        min: 0,
+        max: 180,
+        step: .1,
+        defaultValue: 90,
+        unit: "deg"
+    },
+    invert: {
+        min: 0,
+        max: 1,
+        step: .01,
+        defaultValue: 1,
+        unit: ""
+    }
+
 }
 
 export default filters;
